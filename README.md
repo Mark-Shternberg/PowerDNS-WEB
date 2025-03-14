cf7d95e4f5034c1355c4191a91eaf0883e4363e8
Mark Shternberg <nikita@medvedev-it.com>
2025-03-14 22:49:17 +0300

    Pre-release changes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PowerDNS-Web
-PROJECT IS UNDER DEVELOPMENT
+PowerDNS-Web is a web interface for PowerDNS. It is a WEB application that uses REST API to communicate with PowerDNS server.
+
+# Installation via install script
+
+1. Download last release archive
+2. `mkdir powerdns-web-install && tar -xzvf powerdns-web-v* -C powerdns-web-install && rm powerdns-web-v*`
+3. `cd powerdns-web-install`
+4. Run `sudo chmod +x install.sh && su root ./install.sh`
--- install.sh
+++ install.sh
@@ -8,30 +8,31 @@ if [ $(id -u) -ne 0 ]; then
   exit 1
 fi
 
-if [[ " $@ " =~ " -update " ]]; then
-    if ! systemctl list-units --type=service --all  > /dev/null 2>&1 | grep -q "powerdns-web"; then
-        echo -e "${colRed}PowerDNS-WEB isn't installed. Run script without arguments, Exiting...${resetCol}"
-        exit 0
+check_port_53() {
+    if ss -tuln | grep -q ":53 "; then
+        echo -e "\033[31mError: Port 53 is already in use. Exiting...\033[0m"
+        exit 1
     fi
+}
 
-    if [[ ! -d /var/www/powerdns-web ]]; then
-        echo -e "${colRed}PowerDNS-WEB isn't installed in default directory.\nUpdate program manually, Exiting...${resetCol}"
-        exit 0;
-    fi
-    systemctl stop powerdns-web.service
-    sleep 1
+check_port_53
 
-    find "powerdns-web" -type f ! -name "appsettings.json" -exec cp --parents {} "/var/www/" \;
-
-    systemctl start powerdns-web.service
-    if [ $? -eq 0 ]; then
-        echo -e "${colGreen}\tPowerDNS-WEB upgraded!${resetCol}"
-    else
-        echo -e "$colRed Upgrade error. $resetCol"
-        exit 0 
-    fi
-    exit 0 
+if sudo lsof /var/lib/dpkg/lock-frontend /var/lib/dpkg/lock /var/lib/apt/lists/lock /var/cache/apt/archives/lock > /dev/null 2>&1; then
+    echo -e "\033[31mError: Another package management process is running (dpkg/apt locked). Exiting...\033[0m"
+    exit 1
 fi
+
+if pgrep -x "unattended-upgrade" > /dev/null || pgrep -x "dpkg" > /dev/null || pgrep -x "apt" > /dev/null; then
+    echo -e "\033[31mError: Another package management process (unattended-upgrade, dpkg, or apt) is running. Exiting...\033[0m"
+    exit 1
+fi
+
+read -p "Do you want to install PowerDNS Recursor? [Yy/Nn]: " install_recursor
+case $install_recursor in
+    [yY] ) recursor_enabled="Enabled"; install_recursor_flag=true;;
+    [nN] ) recursor_enabled="Disabled"; install_recursor_flag=false;;
+    * ) echo -e "$colRed Invalid input! Type only Y or N. Exiting...$resetCol"; exit 1;;
+esac
 
 execute_by_distro() {
   if [ -f /etc/os-release ]; then
@@ -53,19 +54,31 @@ execute_by_distro() {
   case "$DISTRO" in
     ubuntu|debian)
       apt -qqq update 
-      apt install mysql-server nginx jq -y || { echo -e "${colRed}Error installing MySQL/NGINX! Exiting...${resetCol}"; exit 1; }
+      apt install mysql-server nginx pdns-server pdns-backend-mysql jq -y || { echo -e "${colRed}Error installing packages! Exiting...${resetCol}"; exit 1; }
+      if [[ "$install_recursor_flag" == "true" ]]; then
+        apt install pdns-recursor -y
+      fi
       ;;
     centos|rhel)
       yum update -q -y
-      yum install mysql-server nginx jq -y || { echo -e "${colRed}Error installing MySQL/NGINX! Exiting...${resetCol}"; exit 1; }
+      yum install mysql-server nginx pdns-server pdns-backend-mysql jq -y || { echo -e "${colRed}Error installing packages! Exiting...${resetCol}"; exit 1; }
+      if [[ "$install_recursor_flag" == "true" ]]; then
+        yum install pdns-recursor -y
+      fi
       ;;
     fedora)
       dnf update -q -y
-      dnf install mysql-server nginx jq -y || { echo -e "${colRed}Error installing MySQL/NGINX! Exiting...${resetCol}"; exit 1; }
+      dnf install mysql-server nginx pdns-server pdns-backend-mysql jq -y || { echo -e "${colRed}Error installing packages! Exiting...${resetCol}"; exit 1; }
+      if [[ "$install_recursor_flag" == "true" ]]; then
+        dnf install pdns-recursor -y
+      fi
       ;;
     arch)
       pacman -Syu --noconfirm
-      pacman -S mariadb nginx jq --noconfirm || { echo -e "${colRed}Error installing MariaDB/NGINX! Exiting...${resetCol}"; exit 1; }
+      pacman -S mariadb nginx pdns-server pdns-backend-mysql jq --noconfirm || { echo -e "${colRed}Error installing packages! Exiting...${resetCol}"; exit 1; }
+      if [[ "$install_recursor_flag" == "true" ]]; then
+        pacman -S pdns-recursor --noconfirm
+      fi
       ;;
     *)
       echo -e "${colRed}Unknown or unsupported Linux distribution: $DISTRO\nExiting...${resetCol}"
@@ -75,7 +88,7 @@ execute_by_distro() {
 }
 
 while true; do
-    echo -n "Will be installed: .NET SDK 8.0, jq, mysql-server and nginx. Ok? [Yy/Nn]: "
+    echo -n "Will be also installed: .NET SDK 8.0, jq, mysql-server, pdns-backend-mysql, pdns-server and nginx. Ok? [Yy/Nn]: "
     read accept
     case $accept in
         [yY] ) break;;
@@ -85,7 +98,14 @@ while true; do
 done
 
 if [[ ! -d /var/www ]]; then
-  mkdir /var/www
+  mkdir -p /var/www
+fi
+if [[ ! -d /var/opt/pdns-rec-api ]]; then
+  mkdir -p /var/opt/pdns-rec-api
+fi
+if [[ -d /var/www/powerdns-web ]]; then
+  echo -e "${colRed}Directory /var/www/powerdns-web already exists! Please remove it manually or use update mode.${resetCol}"
+  exit 1
 fi
 
 mv powerdns-web /var/www/powerdns-web
@@ -98,45 +118,118 @@ else
   echo -e "$colRed Error while installing DotNET. $resetCol"
   exit 0 
 fi
-sleep 2
+sleep 5
 mv /root/.dotnet /var/www/powerdns-web/
-chown -R www-data:www-data /var/www/powerdns-web
-chmod -R 744 /var/www/powerdns-web
 
 DBpassword=$(tr -dc 'A-Za-z0-9!?%=' < /dev/urandom | head -c 10)
+API_KEY=$(tr -dc 'A-Za-z0-9!?%=' < /dev/urandom | head -c 10)
 
 execute_by_distro
 
 read -p "Enter root password for MySQL: " mysql_root_password
 
-mysql -u root -p"$mysql_root_password" << eof
+mysql -u root -p"$(printf '%q' "$mysql_root_password")" << eof
 CREATE DATABASE powerdnsweb;
+CREATE DATABASE powerdns;
 CREATE USER 'powerdnsweb'@'localhost' IDENTIFIED WITH mysql_native_password BY '$DBpassword';
+CREATE USER 'powerdns'@'localhost' IDENTIFIED WITH mysql_native_password BY '$DBpassword';
 GRANT ALL PRIVILEGES ON powerdnsweb.* TO 'powerdnsweb'@'localhost';
+GRANT ALL PRIVILEGES ON powerdns.* TO 'powerdns'@'localhost';
 FLUSH PRIVILEGES;
 eof
 
 JSON_FILE="/var/www/powerdns-web/appsettings.json"
 
 jq --arg server "localhost" \
-   --arg user "cartinvent" \
+   --arg user "powerdnsweb" \
    --arg password "$DBpassword" \
-   --arg database "cartinvent" \
-   '.MySQLConnection.server = $server | 
-    .MySQLConnection.user = $user | 
-    .MySQLConnection.password = $password | 
-    .MySQLConnection.database = $database' \
+   --arg database "powerdnsweb" \
+   --arg apikey "$API_KEY" \
+   --arg recursor_enabled "$recursor_enabled" \
+   '.MySQLConnection.Server = $server | 
+    .MySQLConnection.User = $user | 
+    .MySQLConnection.Password = $password | 
+    .MySQLConnection.Database = $database |
+    .pdns.Api_Key = $apikey |
+    .recursor.Api_Key = $apikey |
+    .recursor.Enabled = $recursor_enabled' \
    "$JSON_FILE" > tmp.$$.json && mv tmp.$$.json "$JSON_FILE"
+
+chown -R www-data:www-data /var/www/powerdns-web
+chmod -R 744 /var/www/powerdns-web
 
 read -p "Enter your server IP/DNS for NGINX: " server_name
 
 rm /etc/nginx/sites-enabled/default
+
+NGINX_CONF="/etc/nginx/sites-enabled/powerdns-web"
+
+if [[ -f $NGINX_CONF ]]; then
+  mv "$NGINX_CONF" "$NGINX_CONF.bak"
+fi
+
 echo -e "server {\n\
 server_name $server_name;\n\
   location / {\n\
     proxy_pass http://localhost:5000;\n\
   }\n\
 }" > /etc/nginx/sites-enabled/powerdns-web
+
+PDNS_CONFIG="/etc/powerdns/pdns.conf"
+
+cat << EOF > "$PDNS_CONFIG"
+launch=gmysql
+gmysql-host=127.0.0.1
+gmysql-dbname=powerdns
+gmysql-user=powerdns
+gmysql-password=$DBpassword
+gmysql-dnssec=yes
+local-port=5300
+allow-dnsupdate-from=127.0.0.0/8,::1,0.0.0.0/0
+api=yes
+api-key=$API_KEY
+dnsupdate=yes
+include-dir=/etc/powerdns/pdns.d
+load-modules=gmysql
+security-poll-suffix=
+webserver=yes
+webserver-address=127.0.0.1
+webserver-allow-from=127.0.0.1,::1
+webserver-port=8081
+EOF
+
+echo -e "${colGreen}PowerDNS configuration written to $PDNS_CONFIG${resetCol}"
+
+if [[ "$install_recursor_flag" == "true" ]]; then
+  RECURSOR_CONFIG="/etc/powerdns/recursor.conf"
+
+  cat << EOF > "$RECURSOR_CONFIG"
+forward-zones=
+forward-zones-recurse=
+api-config-dir=/var/opt/pdns-rec-api
+include-dir=/var/opt/pdns-rec-api
+allow-from=127.0.0.0/8, 10.0.0.0/8, 100.64.0.0/10, 169.254.0.0/16, 192.168.0.0/16, 172.16.0.0/12, ::1/128, fc00::/7, fe80::/10
+api-key=$API_KEY
+config-dir=/etc/powerdns
+hint-file=/usr/share/dns/root.hints
+local-address=0.0.0.0
+local-port=53
+lua-config-file=/etc/powerdns/recursor.lua
+public-suffix-list-file=/usr/share/publicsuffix/public_suffix_list.dat
+quiet=yes
+security-poll-suffix=
+webserver=yes
+webserver-address=127.0.0.1
+webserver-allow-from=127.0.0.1,::1
+EOF
+
+  echo -e "${colGreen}PowerDNS Recursor configuration written to $RECURSOR_CONFIG${resetCol}"
+fi
+
+systemctl restart pdns
+if [[ "$install_recursor_flag" == "true" ]]; then
+  systemctl restart pdns-recursor
+fi
 
 if command -v systemctl > /dev/null 2>&1; then
   echo -e "[Unit]\nDescription=WEB application for PowerDNS server\n\
@@ -153,14 +246,11 @@ WantedBy=multi-user.target" > /etc/systemd/system/powerdns-web.service
   systemctl start powerdns-web.service
 
   if [ $? -eq 0 ]; then
-    systemctl enable powerdns-weby.service
+    systemctl enable powerdns-web.service
     echo -e "${colGreen}\tGreat! powerdns-web service installed and started\n\
 \tNow you can go to: http://$server_name!${resetCol}"
   else
     echo -e "$colRed Error starting service. Check logs! $resetCol"
     exit 0 
   fi
-
-else
-  echo -e "${colRed}\tsystemctl not found! Please configure and start the service manually.${resetCol}"
 fi
